7fbe7cc64650de2f1e0350f64c62779560f65584
tanzir ibne ali <tanziribneali@gmail.com>
2025-03-24 01:23:37 +0600

    project analytics created

--- app/(dashboard)/workspaces/[workspaceId]/projects/[projectId]/client.tsx
+++ app/(dashboard)/workspaces/[workspaceId]/projects/[projectId]/client.tsx
@@ -1,9 +1,11 @@
-"use client"
+"use client";
 
+import Analytics from "@/components/shared/analytics";
 import PageError from "@/components/shared/page-error";
 import PageLoader from "@/components/shared/page-loader";
 import { Button } from "@/components/ui/button";
 import { useGetProject } from "@/features/projects/api/use-get-project";
+import { useGetProjectAnalytics } from "@/features/projects/api/use-get-project-analytics";
 import { ProjectAvatar } from "@/features/projects/components/project-avatar";
 import { useProjectId } from "@/features/projects/hooks/use-project-id";
 import TaskViewSwitcher from "@/features/tasks/components/task-view-switcher";
@@ -11,35 +13,44 @@ import { PencilIcon } from "lucide-react";
 import Link from "next/link";
 
 const ProjectIdClient = () => {
-    const projectId = useProjectId()
-    const {data, isLoading} = useGetProject({projectId})
+  const projectId = useProjectId();
+  const { data: project, isLoading: isLoadingProject } = useGetProject({
+    projectId,
+  });
+  const { data: analytics, isLoading: isLoadingAnalytics } =
+    useGetProjectAnalytics({ projectId });
 
-    if (isLoading) return <PageLoader/>;
-    if (!data) return <PageError message="Project not found"/>;
-    return (
-        <div className='flex flex-col gap-y-4'>
-            <div className='flex items-center justify-between'>
-                <div className='flex items-center gap-x-2'>
-                    <ProjectAvatar
-                    name={data.name}
-                    image={data.imageUrl}
-                    className='size-8'
-                    />
-                    <p className='text-lg font-semibold'>{data.name}</p>
-                </div>
-                <div>
-                    <Button variant={"secondary"} size={"sm"} asChild>
-                        <Link href={`/workspaces/${data.workspaceId}/projects/${data.$id}/settings`}>
-                        <PencilIcon className='size-4 mr-2'/>
-                        Edit Project
-                        </Link>
-                        
-                    </Button>
-                </div>
-            </div>
-            <TaskViewSwitcher hideProjectFilter/>
+  const isLoading = isLoadingProject || isLoadingAnalytics;
+
+  if (isLoading) return <PageLoader />;
+  if (!project || !analytics) return <PageError message="Project not found" />;
+  return (
+    <div className="flex flex-col gap-y-4">
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-x-2">
+          <ProjectAvatar
+            name={project.name}
+            image={project.imageUrl}
+            className="size-8"
+          />
+          <p className="text-lg font-semibold">{project.name}</p>
         </div>
-    );
+        <div>
+          <Button variant={"secondary"} size={"sm"} asChild>
+            <Link
+              href={`/workspaces/${project.workspaceId}/projects/${project.$id}/settings`}
+            >
+              <PencilIcon className="size-4 mr-2" />
+              Edit Project
+            </Link>
+          </Button>
+        </div>
+      </div>
+      {analytics ? <Analytics data={analytics} /> : null}
+
+      <TaskViewSwitcher hideProjectFilter />
+    </div>
+  );
 };
 
 export default ProjectIdClient;
